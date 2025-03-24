26acab3ca7c1d3d93c9f64eb43bd4f95383f4c9d
Emam Uddin <emamskingdom@gmail.com>
2025-03-24 23:51:39 +0600

    project done also

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import exercise from '../assets/exercise.jpg.jpg'
+
+const Home = () => {
+    return (
+        <div>
+            <img src={exercise} alt="" />
+        </div>
+    );
+};
+
+export default Home;
