6e24de63b0df39136e247c67349fa604dc8466ff
Emam Uddin <emamskingdom@gmail.com>
2025-03-24 23:51:20 +0600

    setup routerr

--- src/routers/router.jsx
+++ src/routers/router.jsx
@@ -1,11 +1,36 @@
 import { createBrowserRouter } from "react-router-dom";
+import HomeLayout from "../layouts/HomeLayout";
+import ErrorPage from "../pages/ErrorPage";
+import AddSchedule from "../pages/AddSchedule";
+import Home from "../pages/Home";
+import SignIn from "../pages/SignIn";
+import SignUp from "../pages/SignUp";
 
 
 const router =createBrowserRouter([
     {
       path: "/",
-      element: <div>Hello world!</div>,
+      element: <HomeLayout></HomeLayout>,
+      errorElement:<ErrorPage></ErrorPage>,
+      children:[
+        {
+          path:'/',
+          element:<Home></Home>
+        },
+        {
+          path:"addSchedule",
+          element:<AddSchedule></AddSchedule>
+        }
+      ]
     },
+    {
+      path:'/singIn',
+      element:<SignIn></SignIn>
+    },
+    {
+      path:'/signUp',
+      element:<SignUp></SignUp>
+    }
   ])
 
 
