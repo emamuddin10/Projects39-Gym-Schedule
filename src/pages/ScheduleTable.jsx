872a70037fd19177ecb90f6ee55fe713269f40a0
Emam Uddin <emamskingdom@gmail.com>
2025-03-29 03:42:59 +0600

    schedu;etable

--- src/pages/ScheduleTable.jsx
+++ src/pages/ScheduleTable.jsx
@@ -4,13 +4,9 @@ import { Link } from "react-router-dom";
 import Swal from "sweetalert2";
 
 const ScheduleTable = ({schedule,idx,scheduleData,setScheduleData}) => {
-  console.log(scheduleData)
-    const {_id,title,date,day,time}=schedule
-    console.log(schedule)
-  const isComplate = false;
+  console.log('schedule',schedule)
+    const {_id,title,date,day,time,isCompleted}=schedule
 
-  
-      
 
   const handleDelete = id =>{
     fetch(`http://localhost:5000/schedule/${id}`,{
@@ -32,29 +28,52 @@ const ScheduleTable = ({schedule,idx,scheduleData,setScheduleData}) => {
       console.log(scheduleData)
     })
   }
+  const handleUpdateStatus = id =>{
+    
+    fetch(`http://localhost:5000/status/${id}`,{
+      method:'PATCH'
+    })
+    .then(res=>res.json())
+    .then(data=>{
+      console.log(console.log(data))
+      const newData = scheduleData.map((schedule)=> 
+        schedule._id === id ? {...schedule,isCompleted:true}:schedule)
+
+      if(data.deletedCount){
+        Swal.fire({
+                  title: "Modified Data!",
+                  text: "This work is complated !",
+                  icon: "success"
+                });
+      }
+      setScheduleData(newData)
+
+     
+    })
+  }
   return (
     <>
       <tr >
-        <td className="border-1 border-amber-300 p-3 text-center">{idx + 1}</td>
-        <td className="border-1 border-amber-300 p-3 text-center">{title}</td>
-        <td className="border-1 border-amber-300 p-3 text-center">{date}</td>
-        <td className="border-1 border-amber-300 p-3 text-center">{day}</td>
-        <td className="border-1 border-amber-300 p-3 text-center">{time}</td>
-        <td className="border-1 border-amber-300 p-3 text-center">
-          <div>
+        <td className="border-1 border-slate-300 p-3 text-center">{idx + 1}</td>
+        <td className="border-1 border-slate-300 p-3 text-center">{title}</td>
+        <td className="border-1 border-slate-300 p-3 text-center">{date}</td>
+        <td className="border-1 border-slate-300 p-3 text-center">{day}</td>
+        <td className="border-1 border-slate-300 p-3 text-center">{time}</td>
+        <td className="border-1 border-slate-300 p-3 text-center">
+          <div className="flex gap-3">
             {""}
-            <button onClick={()=>handleDelete(_id)}>
+            <button className="bg-slate-100 p-2 rounded-lg" onClick={()=>handleDelete(_id)}>
               <MdDelete className="text-pink-500 text-2xl" />
             </button>
-            <button>
+            <button className="bg-slate-100 p-2 rounded-lg">
               
               <Link to={`/update/${_id}`}>
               {""}
                 <MdEditDocument className="text-pink-500 text-2xl" />
               </Link>
             </button>
-            <button>
-              {isComplate ? (
+            <button className="bg-slate-100 p-2 rounded-lg" onClick={()=>handleUpdateStatus(_id)}>
+              {isCompleted ? (
                 <MdDoneAll className="text-pink-500 text-2xl" />
               ) : (
                 <MdDone className="text-pink-500 text-2xl" />
