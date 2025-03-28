e3165f36442b3f8ee9e72a6ec1c041fc012ac81c
Emam Uddin <emamskingdom@gmail.com>
2025-03-28 21:22:11 +0600

    schedule

---
+++ src/pages/Schedule.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import { useLoaderData } from "react-router-dom";
+import ScheduleTable from "./ScheduleTable";
+
+const Schedule = () => {
+    const loadedData = useLoaderData()
+    const [scheduleData,setScheduleData]=useState(loadedData)
+   
+  return (
+    <div className="flex justify-center items-center flex-col">
+      <div>
+        <input
+         className="boder-2 border-pink-500 rounded-full outline-1 outline-pink-500 px-10 py-3 my-5 w-full"
+          type="text"
+          name="search"
+          id=""
+          placeholder="search"
+        //   onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
+      
+      <div>
+        <div>
+           <table>
+             <thead>
+                <tr className="border-2 border-pink-500">
+                    <th className="border-2 border-pink-500 px-10 py-3">Seriul</th>
+                    <th className="border-2 border-pink-500 px-10 py-3">Title</th>
+                    <th className="border-2 border-pink-500 px-10 py-3">Date</th>
+                    <th className="border-2 border-pink-500 px-10 py-3">Day</th>
+                    <th className="border-2 border-pink-500 px-10 py-3" >Time</th>
+                    <th className="border-2 border-pink-500 px-10 py-3">Action</th>
+                </tr>
+             </thead>
+             <tbody>
+                 {
+                    scheduleData.length===0 ? (<p>No Data Found</p>) : (
+                       scheduleData.map((schedule,index) => <ScheduleTable
+                        key={schedule._id} 
+                        idx={index} 
+                        schedule={schedule}
+                        scheduleData={scheduleData}
+                        setScheduleData={setScheduleData}
+                        
+                        >
+
+                        </ScheduleTable>)
+                    )
+                 }
+             </tbody>
+           </table>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Schedule;
